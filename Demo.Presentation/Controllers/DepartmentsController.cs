7344bbeb6e40a4fdca3dd2385262bbc621ac7c4b
salma1hosam <salma.hossam2003@gmail.com>
2025-03-24 00:33:49 +0200

    Department Controller Index

---
+++ Demo.Presentation/Controllers/DepartmentsController.cs
@@ -0,0 +1,16 @@
+﻿using Demo.BusinessLogic.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Presentation.Controllers
+{
+	public class DepartmentsController(IDepartmentService _departmentService) : Controller
+	{
+		//GET : BaseURL/Departments/Index
+		[HttpGet] //Default
+		public IActionResult Index()
+		{
+			var departments = _departmentService.GetAllDepartments();
+			return View(departments);
+		}
+	}
+}
