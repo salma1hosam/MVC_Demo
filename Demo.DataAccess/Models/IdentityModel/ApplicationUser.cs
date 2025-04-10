c40369184b6c0abba635eeaca6872217a3c18159
salma1hosam <salma.hossam2003@gmail.com>
2025-04-10 14:56:54 +0200

    Install Package and DbContext

---
+++ Demo.DataAccess/Models/IdentityModel/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Demo.DataAccess.Models.IdentityModel
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; } = null!;
+        public string? LastName { get; set; }
+    }
+}
