d3ecc2311711fda9552f9321a0cddba8365c3675
salma1hosam <salma.hossam2003@gmail.com>
2025-04-18 14:06:04 +0200

    User Manager Module - Index Action

---
+++ Demo.Presentation/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using Demo.DataAccess.Models.IdentityModel;
+using Demo.Presentation.ViewModels.UserViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Demo.Presentation.Controllers
+{
+	public class UsersController(UserManager<ApplicationUser> _userManager) : Controller
+	{
+		public IActionResult Index()
+		{
+			var users = _userManager.Users.ToList();			
+			List<UserViewModel> usersViewModel = [];
+			foreach (var user in users)
+			{
+				var viewModel = new UserViewModel
+				{
+					Id = user.Id,
+					FirstName = user.FirstName,
+					LastName = user.LastName,
+					Email = user.Email,
+					PhoneNumber = user.PhoneNumber
+					// Role
+				};
+
+				usersViewModel.Add(viewModel);
+			}
+			return View(usersViewModel);
+		}
+	}
+}
