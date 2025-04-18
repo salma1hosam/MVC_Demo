ba0cac2e6a95547626d9649dd4934802286ee7e8
salma1hosam <salma.hossam2003@gmail.com>
2025-04-19 01:36:36 +0200

    User Manager Module - Edit Action

--- Demo.Presentation/Controllers/UsersController.cs
+++ Demo.Presentation/Controllers/UsersController.cs
@@ -1,4 +1,6 @@
-﻿using Demo.DataAccess.Models.IdentityModel;
+﻿using Demo.BusinessLogic.DataTransferObjects.Employee;
+using Demo.DataAccess.Models.IdentityModel;
+using Demo.Presentation.ViewModels.EmployeeViewModels;
 using Demo.Presentation.ViewModels.UserViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -6,8 +8,9 @@ using Microsoft.IdentityModel.Tokens;
 
 namespace Demo.Presentation.Controllers
 {
-	public class UsersController(UserManager<ApplicationUser> _userManager) : Controller
+	public class UsersController(UserManager<ApplicationUser> _userManager , ILogger<UsersController> _logger , IWebHostEnvironment _environment) : Controller
 	{
+		[HttpGet]
 		public IActionResult Index()
 		{
 			var users = _userManager.Users.ToList();			
@@ -28,5 +31,61 @@ namespace Demo.Presentation.Controllers
 			}
 			return View(usersViewModel);
 		}
+
+		#region Edit User
+
+		[HttpGet]
+		public IActionResult Edit(string? id)
+		{
+			if (id.IsNullOrEmpty()) return BadRequest();
+			var user = _userManager.FindByIdAsync(id).Result;
+			if (user is null) return NotFound();
+
+			var userEditViewModel = new UserEditViewModel()
+			{
+				FirstName = user.FirstName,
+				LastName = user.LastName,
+				PhoneNumber = user.PhoneNumber
+			};
+			return View(userEditViewModel);
+		}
+
+		[HttpPost]
+		public IActionResult Edit([FromRoute] string? id, UserEditViewModel userEditViewModel)
+		{
+			if (id.IsNullOrEmpty()) return BadRequest();
+			if (!ModelState.IsValid) return View(userEditViewModel);
+			try
+			{
+				var user = _userManager.FindByIdAsync(id).Result;
+				user.FirstName = userEditViewModel.FirstName;
+				user.LastName = userEditViewModel.LastName;
+				user.PhoneNumber = userEditViewModel.PhoneNumber;
+
+				var result = _userManager.UpdateAsync(user).Result;
+				if (result.Succeeded)
+					return RedirectToAction(nameof(Index));
+				else
+				{
+					foreach (var error in result.Errors)
+						ModelState.AddModelError(string.Empty, error.Description);
+					return View(userEditViewModel);
+				}
+			}
+			catch (Exception ex)
+			{
+				if (_environment.IsDevelopment())
+				{
+					ModelState.AddModelError(string.Empty, ex.Message);
+					return View(userEditViewModel);
+				}
+				else
+				{
+					_logger.LogError(ex.Message);
+					return View("ErrorView", ex);
+				}
+			}
+		}
+		#endregion
 	}
 }
