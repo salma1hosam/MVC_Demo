a3e3f86c2a0204e864054385fef017992e154b81
salma1hosam <salma.hossam2003@gmail.com>
2025-04-19 02:31:43 +0200

    User Manager Module - Delete Action

--- Demo.Presentation/Controllers/UsersController.cs
+++ Demo.Presentation/Controllers/UsersController.cs
@@ -104,5 +104,58 @@ namespace Demo.Presentation.Controllers
 			};
 			return View(userViewModel);
 		}
+
+		#region Delete User
+
+		[HttpGet]
+		public IActionResult Delete(string? id)
+		{
+			if (id.IsNullOrEmpty()) return BadRequest();
+			var user = _userManager.FindByIdAsync(id).Result;
+			if (user is null) return NotFound();
+			var userViewModel = new UserDetailsViewModel()
+			{
+				Id = id,
+				FirstName = user.FirstName,
+				LastName = user.LastName,
+				PhoneNumber = user.PhoneNumber,
+				Email = user.Email
+			};
+			return View(userViewModel);
+		}
+
+		[HttpPost]
+		public IActionResult DeleteUser(string id)
+		{
+			if (id.IsNullOrEmpty()) return BadRequest();
+			try
+			{
+				var user = _userManager.FindByIdAsync(id).Result;
+				if (user is null) return NotFound();
+				var result = _userManager.DeleteAsync(user).Result;
+				if (result.Succeeded)
+					return RedirectToAction(nameof(Index));
+				else
+				{
+					foreach (var error in result.Errors)
+						ModelState.AddModelError(string.Empty, error.Description);
+					return RedirectToAction(nameof(Delete), new { Id = id });
+				}
+			}
+			catch (Exception ex)
+			{
+				if (_environment.IsDevelopment())
+				{
+					ModelState.AddModelError(string.Empty, ex.Message);
+					return RedirectToAction(nameof(Delete), new { Id = id });
+				}
+				else
+				{
+					_logger.LogError(ex.Message);
+					return View("ErrorView", ex);
+				}
+			}
+		}
+		#endregion
 	}
 }
