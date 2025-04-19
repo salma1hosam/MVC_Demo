f64d31b4f719397af34aec7865773aa3ec110dc6
salma1hosam <salma.hossam2003@gmail.com>
2025-04-19 03:47:19 +0200

    Role Manager - Create Action

--- Demo.Presentation/Controllers/RolesController.cs
+++ Demo.Presentation/Controllers/RolesController.cs
@@ -1,6 +1,8 @@
-﻿using Demo.Presentation.ViewModels.RoleViewModels;
+﻿using Demo.Presentation.ViewModels.AccountViewModels;
+using Demo.Presentation.ViewModels.RoleViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Demo.Presentation.Controllers
 {
@@ -10,7 +12,7 @@ namespace Demo.Presentation.Controllers
 		{
 			var roles = _roleManager.Roles.ToList();
 			var rolesViewModel = new List<RoleViewModel>();
-			foreach(var role in roles)
+			foreach (var role in roles)
 			{
 				var viewModel = new RoleViewModel()
 				{
@@ -21,5 +23,29 @@ namespace Demo.Presentation.Controllers
 			}
 			return View(rolesViewModel);
 		}
+
+		#region Create Role
+		[HttpGet]
+		public IActionResult Create() => View();
+
+		[HttpPost]
+		public IActionResult Create(CreateRoleViewModel createRoleViewModel)
+		{
+			if (!ModelState.IsValid) return View(createRoleViewModel);
+			var role = new IdentityRole()
+			{
+				Name = createRoleViewModel.RoleName
+			};
+			var result = _roleManager.CreateAsync(role).Result;
+			if (result.Succeeded)
+				return RedirectToAction(nameof(Index));
+			else
+			{
+				foreach (var error in result.Errors)
+					ModelState.AddModelError(string.Empty, error.Description);
+				return View(createRoleViewModel);
+			}
+		}
+		#endregion
 	}
 }
